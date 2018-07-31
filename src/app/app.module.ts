46092ef605334ae897a3dd571473e113b4d4c5e4
Kedfy Gonzalez <kedfygo@gmail.com>
2018-07-31 16:55:44 -0500

    Fin seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule  } from '@angular/common/http';
 
 // Rutas
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +15,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +27,9 @@ import { ItemComponent } from './pages/item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
