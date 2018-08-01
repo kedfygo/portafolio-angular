4aa9572b471348856c83607d8ad7512da76025c7
Kedfy Gonzalez <kedfygo@gmail.com>
2018-07-31 19:21:44 -0500

    Fin seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,9 +9,11 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any;
 
   constructor( private _http: HttpClient) {
     console.log('Servicio Listo');
+    this.cargarEquipo();
 
     // Leer el archvivo JSON
     this._http.get('assets/data/data-pagina.json')
@@ -20,6 +22,27 @@ export class InfoPaginaService {
                 this.info = resp;
                 console.log(resp);
                 // console.log(resp['twitter']);
+
+              });
+  }
+
+  private cargarInfo(){
+    this._http.get('assets/data/data-pagina.json')
+              .subscribe((resp: InfoPagina) => {
+                this.cargada = true;
+                this.info = resp;
+
+              });
+
+
+
+  }
+
+  private cargarEquipo() {
+    this._http.get('https://angular-html-fdef1.firebaseio.com/equipo.json')
+              .subscribe((resp: any[] ) => {
+                this.equipo = resp;
+                console.log(resp);
 
               });
   }
